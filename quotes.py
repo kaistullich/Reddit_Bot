3ceb21a4de9c802ea3a05a6a0842901a9ebca8ac
kaistullich <kairstullich@gmail.com>
2017-04-26 16:16:51 -0700

    initial quotes commit

---
+++ quotes.py
@@ -0,0 +1,23 @@
+# SOURCE OF QUOTES: http://www.quotery.com/lists/100-funny-quotes-worth-laughing-over/
+
+replies = [
+    "When I die, I want to go peacefully like my grandfather did–in his sleep. Not yelling and screaming like the passengers in his car.",
+    "I have six locks on my door all in a row. When I go out, I lock every other one. I figure no matter how long somebody stands there picking the locks, they are always locking three.",
+    "Always borrow money from a pessimist. He won’t expect it back.",
+    "Friendship is like peeing on yourself: everyone can see it, but only you get the warm feeling that it brings.",
+    "First the doctor told me the good news: I was going to have a disease named after me.",
+    "A successful man is one who makes more money than his wife can spend. A successful woman is one who can find such a man.",
+    "How do you get a sweet little 80-year-old lady to say the F word? Get another sweet little 80-year-old lady to yell “BINGO!”",
+    "My therapist told me the way to achieve true inner peace is to finish what I start. So far I’ve finished two bags of M&Ms and a chocolate cake. I feel better already.",
+    "Dogs have masters. Cats have staff.",
+    "Knowledge is knowing a tomato is a fruit; wisdom is not putting it in a fruit salad.",
+    'Why do people say “no offense” right before they’re about to offend you?',
+    'I love deadlines. I like the whooshing sound they make as they fly by.',
+    'By all means, marry. If you get a good wife, you’ll become happy; if you get a bad one, you’ll become a philosopher.',
+    'I asked God for a bike, but I know God doesn’t work that way. So I stole a bike and asked for forgiveness.',
+    'The best way to lie is to tell the truth . . . carefully edited truth.',
+    'Do not argue with an idiot. He will drag you down to his level and beat you with experience.',
+    'The only mystery in life is why the kamikaze pilots wore helmets.',
+    'Going to church doesn’t make you a Christian any more than standing in a garage makes you a car.',
+    'Men have only two emotions: hungry and horny. If you see him without an erection, make him a sandwich.'
+]
