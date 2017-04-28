0fc8c90ca307610d3d8478f562f63cbebed49ba7
kaistullich <kairstullich@gmail.com>
2017-04-27 19:26:43 -0700

    initial words commit

---
+++ words.py
@@ -0,0 +1,5 @@
+# Words generated from curse_words_scraper.py
+words = {
+    'bastard', 'crap', 'slut', 'cock', 'fag', 'arsehole', 'bloody', 'bollocks', 'shit', 'douche',
+    'bitch', 'darn', 'dick', 'piss', 'fuck', 'asshole', 'bugger', 'cunt', 'pussy', 'damn',
+}
