18ab37bb1b773b8ad88952b66d1264742029e5a2
kaistullich <kairstullich@gmail.com>
2017-04-27 18:55:24 -0700

    try/except block around submission.reply

--- bot.py
+++ bot.py
@@ -1,12 +1,24 @@
+# Subbredit: https://www.reddit.com/r/pythonforengineers
+
+import json
 import os
 import random
 import re
+import time
 
 import praw
 
 from quotes import quote_replies
 
-reddit = praw.Reddit('bot1')
+with open('config.json') as f:
+    config = json.load(f)
+
+reddit = praw.Reddit(user_agent=config['user_agent'],
+                     client_id=config['client_id'],
+                     client_secret=config['client_secret'],
+                     username=config['username'],
+                     password=config['password']
+                     )
 
 subreddit = reddit.subreddit('pythonforengineers')
 
@@ -27,21 +39,25 @@ else:
         posts_replied_to = list(filter(None, posts_replied_to))
 
 # Loop through `hot` posts (limit to 5)
-for submission in subreddit.new(limit=15):
+for submission in subreddit.new(limit=5):
     # Check if post ID is inside of file already
     if submission.id not in posts_replied_to:
         # Check title of post contains `i love python`
         if re.search("i love python", submission.title, re.IGNORECASE):
             # Reply to post
-            submission.reply('Useless Quote Bot: ' + random.choice(quote_replies))
+            try:
+                submission.reply('Useless Quote Bot: ' + random.choice(quote_replies))
+            except BaseException:
+                print('** RATE LIMITED! **')
             # Console log which post was replied to
             print("Bot replying to : ", submission.title)
             # Append post ID to list
             posts_replied_to.append(submission.id)
+            time.sleep(5)
 
 # Open txt file
 with open("posts_replied_to.txt", "w") as f:
-    # Loop through list
-    for post_id in posts_replied_to:
-        # Write post ID's to txt file
-        f.write(post_id + "\n")
+    # Loop through list with Post IDs
+    for post in posts_replied_to:
+            # Write Post IDs to txt file
+            f.write(post + '\n')
