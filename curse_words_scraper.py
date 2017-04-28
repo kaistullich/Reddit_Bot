2faf3990f9f6c0c5831914f0561d8f10c7111c99
kaistullich <kairstullich@gmail.com>
2017-04-28 11:28:11 -0700

    created new function for deadspin.com & made edits to parse_list()

--- curse_words_scraper.py
+++ curse_words_scraper.py
@@ -39,19 +39,57 @@ def slate_curse_scraper():
         print('Connection not successful with URL')
 
 
-def parse_set(l):
+def deadspin_curse_scraper():
+    # Send off request
+    r = requests.get('http://deadspin.com/behold-the-ultimate-curse-word-bracket-457043269')
+
+    # Check if requests status is 200
+    if r.status_code == 200:
+        print('** Website Online **')
+        # All HTML content
+        html = r.text
+        # Create soup object
+        soup = BeautifulSoup(html, 'lxml')
+        # Find all `ol` tags
+        ol = soup.find_all('ol')
+
+        # Empty cuss_words list
+        cuss_words = list()
+        # Loop through all `ol` tags
+        for el in ol:
+            # Store all `li` tags
+            li_data = el.find_all('li')
+            # Loop through all `li` tags
+            for li in li_data:
+                # Append words to list
+                cuss_words.append(li.text)
+
+        return cuss_words
+
+
+def parse_list(l):
     # Loop through list
     for word in l:
         # Check if strings are numbers or capitalized
         if word.istitle() or word.isdigit():
             # Remove strings if matching criterion
             l.remove(word)
+    # Make all words lowercase
+    lowered_l = [x.lower() for x in l]
     # Store list inside of set to remove doubles
-    completed_set = set(filter(None, l))
+    completed_set = set(filter(None, lowered_l))
     # Return filtered list/set
     return completed_set
 
 
 if __name__ == '__main__':
-    unfiltered_set = slate_curse_scraper()
-    slate_filtered_set = parse_set(unfiltered_set)
+    # Slate.com Words
+    slate_unfiltered_list = slate_curse_scraper()
+    slate_filtered_set = parse_list(slate_unfiltered_list)
+    # Deadspin.com Words
+    deadspin_unfiltered_list = deadspin_curse_scraper()
+    deadspin_filtered_set = parse_list(deadspin_unfiltered_list)
+    # Combine both sets together to remove doubles
+    complete_cuss_set = slate_filtered_set.union(deadspin_filtered_set)
+
+    print(complete_cuss_set)
