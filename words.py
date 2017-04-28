12954ea2a80acf4bd5b59428773a4bb8621e0150
kaistullich <kairstullich@gmail.com>
2017-04-28 11:28:25 -0700

    updates words.py with newest set

--- words.py
+++ words.py
@@ -1,5 +1,8 @@
 # Words generated from curse_words_scraper.py
-words = {
-    'bastard', 'crap', 'slut', 'cock', 'fag', 'arsehole', 'bloody', 'bollocks', 'shit', 'douche',
-    'bitch', 'darn', 'dick', 'piss', 'fuck', 'asshole', 'bugger', 'cunt', 'pussy', 'damn',
+words = {'clusterfuck', 'fag', 'crap', 'vag', 'jizz', 'cunt', 'cocksucker', 'prick', 'bugger', 'poontang', 'motherfucker',
+         'buttfuck', 'dildo', 'dickhead', 'asshole', 'dick', 'jesus christ', 'douche', 'cock', 'clit', 'bastard', 'taint',
+         'bollocks', 'balls', 'cameltoe', 'spooge', 'blowjob', 'asshat', 'arsehole', 'fuckface', 'slut', 'arse', 'shit',
+         'bitch', 'piss', 'tits', 'pussy', 'damn', 'choad', 'fuck', 'pecker', 'bullshit', 'hard-on', 'hell', 'bloody',
+         'nuts', 'darn', 'feltch'
 }
+
